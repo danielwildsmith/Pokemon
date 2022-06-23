581c2cb49f3586a7057226ab974cba9386744793
Daniel Wildsmith <danielwildsmith@ufl.edu>
2022-06-23 11:16:34 -0400

    Added tiled files.

---
+++ map/Tiled/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="collision" tilewidth="12" tileheight="12" tilecount="2291" columns="79">
+ <image source="../../Downloads/collision.jpg" width="950" height="350"/>
+</tileset>
